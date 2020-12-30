f337b7e4eaa9ddca64693508728c2942108ede36
Rojar Smith <rojarsmith@gmail.com>
2020-12-30 16:50:53 +0800

    Add loglevel.

--- client-web/src/index.js
+++ client-web/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import log from 'loglevel';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+console.info("NODE_ENV=" + process.env.NODE_ENV);
+console.info("REACT_APP_ENVIRONMENT=" + process.env.REACT_APP_ENVIRONMENT);
+
+if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
+  log.setLevel("info");
+  // For Debug.
+  // localStorage.removeItem("user");
+} else {
+  // Disable log at production
+  console.log = console.error = console.warn = function () { };
+  log.disableAll(true);
+}
+
+log.info("[index]: Begin ReactDOM.");
 
 ReactDOM.render(
   <React.StrictMode>
