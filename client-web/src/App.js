24531fd4db7d34dfce8d16f305969bb914c659de
Rojar Smith <rojarsmith@gmail.com>
2020-12-31 09:43:40 +0800

    Integrate Router, Switch.

--- client-web/src/App.js
+++ client-web/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import log from "loglevel";
+import { createBrowserHistory } from 'history';
+import { Router, Route, Switch } from 'react-router-dom';
 
 function App() {
+  log.info(`[App]: Rendering App Component`);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={createBrowserHistory()}>
+      {
+        <Switch>
+          
+        </Switch>
+      }
+    </Router>
   );
 }
 
