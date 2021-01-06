6b76b6270ae1f7424aed75291d1b696a4c122096
Rojar Smith <rojarsmith@gmail.com>
2021-01-06 16:50:37 +0800

    Begin develop the component Header.

---
+++ client-web/src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+import log from "loglevel";
+
+export default function Header(props) {
+    log.info(`[NavBar]: Rendering Head Component`)
+    return(
+        <>
+        </>
+    )
+}
