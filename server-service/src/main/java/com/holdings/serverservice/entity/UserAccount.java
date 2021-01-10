018a9ced9cf19444348041e93f05a76ba6ff7e55
Rojar Smith <rojarsmith@gmail.com>
2021-01-10 12:43:47 +0800

    Add columns of privacy in the user account.

--- server-service/src/main/java/com/holdings/serverservice/entity/UserAccount.java
+++ server-service/src/main/java/com/holdings/serverservice/entity/UserAccount.java
@@ -2,11 +2,14 @@ package com.holdings.serverservice.entity;
 
 import java.time.Instant;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.validation.constraints.Email;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -16,27 +19,41 @@ import lombok.Data;
 @Data
 @Entity
 public class UserAccount {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(nullable = false)
-    private String userName;
+	@Column(nullable = false)
+	private String userName;
 
-    @JsonIgnore
-    private String password;
-    
-    @Email
-    @Column(nullable = false)
-    private String email;
+	@JsonIgnore
+	private String password;
 
-    @Column(nullable = false)
-    private Boolean emailVerified = false;
+	@Email
+	@Column(nullable = false)
+	private String email;
+
+	@Column(nullable = false)
+	private Boolean emailVerified = false;
+
+	private Instant registerTime;
+
+	private String registerIp;
+
+	@Column(nullable = false)
+	private Boolean actived = false;
+	
+	// region Privacy
+    private String realName;
     
-    private Instant registerTime;
+    private String company;
     
-    private String registerIp;
+    private String job;
     
-    @Column(nullable = false)
-    private Boolean actived = false;
+    private String taxcode;
+      
+    private String address;
+    
+    private String phone;
+	// endregion
 }
