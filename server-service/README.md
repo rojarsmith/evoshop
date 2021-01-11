fca0c8f7199a2e76e9a51b90d05c198dee617074
Rojar Smith <rojarsmith@gmail.com>
2021-01-11 10:50:35 +0800

    Write new user to database. Add the utility of IP.

---
+++ server-service/README.md
@@ -0,0 +1,17 @@
+# server-service
+
+## Develop with Eclipse IDE
+
+Program arguments:
+
+```bash
+# Windows
+--spring.profiles.active=dev --spring.config.additional-location="E:\\backend\\"
+```
+
+
+
+## ISSUE
+
+
+
--- server-service/src/main/java/com/holdings/serverservice/controller/UserController.java
+++ server-service/src/main/java/com/holdings/serverservice/controller/UserController.java
@@ -1,19 +1,24 @@
 package com.holdings.serverservice.controller;
 
+import java.time.Instant;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.holdings.serverservice.entity.UserAccount;
 import com.holdings.serverservice.payload.UserAccountCreationRequest;
 import com.holdings.serverservice.repository.UserAccountRepository;
+import com.holdings.serverservice.utility.IpUtils;
 import com.holdings.serverservice.utility.ValueValidate;
 
 @RestController
@@ -22,6 +27,9 @@ public class UserController {
 	@Autowired
 	private UserAccountRepository userAccountRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
 	@PostMapping(value = { "/v{version:\\d}/user/signup" })
 	public ResponseEntity<?> userSignup(HttpServletRequest request,
 			@Valid @RequestBody UserAccountCreationRequest userAccountCreationRequest) throws Exception {
@@ -44,7 +52,19 @@ public class UserController {
         if (userAccountRepository.existsByEmail(userAccountCreationRequest.getEmail())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email address already in use.");
         }
-
+        
+        // Creating user's account
+        UserAccount user = new UserAccount();
+        
+        user.setUserName(userAccountCreationRequest.getUserName());
+        user.setPassword(passwordEncoder.encode(userAccountCreationRequest.getPassword()));
+        user.setEmail(userAccountCreationRequest.getEmail());
+        user.setEmailVerified(false);
+        user.setRegisterTime(Instant.now());
+        user.setRegisterIp(IpUtils.getIpAddr(request));
+        user.setActived(true);
+        UserAccount result = userAccountRepository.save(user);
+        
 		return ResponseEntity.created(null).body(null);
 	}
 }
